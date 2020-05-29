474e851251f0ea7d590d93042dd1dda3993eaf2b
OISHI Masakuni <yamasa@bsdhouse.org>
2020-05-29 17:57:04 +0900

    Use Hilt in DfmActivity.
    Unfortunately, this crashes with the following exception:
    ```
         Caused by: java.lang.ClassCastException: com.example.hilt_dfm.DaggerMyApplication_HiltComponents_ApplicationC$ActivityRetainedCImpl$ActivityCImpl cannot be cast to com.example.hilt_dfm.feature.DfmActivity_GeneratedInjector
            at com.example.hilt_dfm.feature.Hilt_DfmActivity.inject(Hilt_DfmActivity.java:60)
            at com.example.hilt_dfm.feature.Hilt_DfmActivity.onCreate(Hilt_DfmActivity.java:35)
            at com.example.hilt_dfm.feature.DfmActivity.onCreate(DfmActivity.kt:17)
            at android.app.Activity.performCreate(Activity.java:7825)
    ```

--- feature/src/main/java/com/example/hilt_dfm/feature/DfmActivity.kt
+++ feature/src/main/java/com/example/hilt_dfm/feature/DfmActivity.kt
@@ -3,14 +3,21 @@ package com.example.hilt_dfm.feature
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.hilt_dfm.SampleRepository
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class DfmActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var sampleRepository: SampleRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dfm)
 
         val messageText: TextView = findViewById(R.id.message_text)
-        messageText.text = "TODO"
+        messageText.text = sampleRepository.getMessage()
     }
 }
